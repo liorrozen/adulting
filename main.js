0d1c3a925239967ad7795a09b0d2f837e619f8f6
Lixonia <lixroz@gmail.com>
2018-10-05 12:07:05 +0300

    clock testing

---
+++ main.js
@@ -0,0 +1,22 @@
+var interval;
+function start() {
+    const maxTime = 3600
+    const actualMax = 360
+    const increment = 10;
+    var time = 0
+
+    interval = setInterval(function() {
+        time = time < maxTime ? time+increment : increment
+
+        actualTime = (time / maxTime) * actualMax
+        actualTime = Math.floor(actualTime)
+        document.getElementById("hand").style.transform = `rotate(${actualTime}deg)`;
+        console.log(time, `rotate(${actualTime}deg)`)
+    }, 1000)
+    document.getElementById("start").disabled = true
+}
+
+function stop() {
+    document.getElementById("start").disabled = false
+    clearInterval(interval)
+}
